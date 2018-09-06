5444b20fcf595ab87b0b09781c3e56b917d351d3
Morgan Newman <mpnewman95@gmail.com>
2018-09-06 11:50:13 -0700

    Moved /api/notes to a router

---
+++ router/notes.router.js
@@ -0,0 +1,68 @@
+/* * * * * * * * * *
+ * /NOTES/ ROUTER  *
+ * * * * * * * * * */
+
+const express = require('express');
+const router = express.Router();
+
+// Expose our notes DB interface
+const data = require('../db/notes');
+const notes = require('../db/simDB').initialize(data); 
+
+// Parse incoming JSON
+router.use(express.json());
+
+router.get('/', (req, res, next) => {
+  // Fetch searchTerm query from client request
+  const {searchTerm} = req.query;
+  // Check if a search was requested
+  notes.filter(searchTerm, (err, list) => {
+    // Throw any errors
+    if (err) next(err);
+    // Send a response if no errors
+    else res.json(list);
+  });
+});
+
+router.get('/:id', (req, res, next) => {
+  // fetch the ID of the requested item
+  const id = req.params.id;
+  // Locate the item in our database
+  notes.find(id, (err, item) => {
+    // Throw any errors
+    if (err) next(err);
+    // If item is found, respond to request
+    else if (item !== undefined) res.json(item);
+    // Otherwise, return 404
+    else next();
+  });
+});
+
+// Think about composing a function that returns a doctorObject() function
+  // Think of composition as combining abilities
+  // Factory function pattern?
+router.put('/:id', (req, res, next) => {
+  // grab the ID from the request
+  const id = req.params.id;
+  // Create the object which will update our DB
+  const updateObj = {};
+  // Set the legal update fields
+  const updateFields = ['title', 'content'];
+  // Iterate through legal update fields
+  updateFields.forEach(field => {
+    // If the field is in our request
+    if (field in req.body) {
+      // Push the value of the field into our update obj
+      updateObj[field] = req.body[field];
+    }
+  });
+  // update the DB with user input
+  notes.update(id, updateObj, (err, item) => {
+    if (err) next(err);
+    else if (item !== undefined) res.json(item);
+    else next();
+  });
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,7 @@
 const {PORT} = require('./config');
 const morgan = require('morgan');
+const notesRouter = require('./router/notes.router');
 
-// Load array of notes
-const data = require('./db/notes');
-// const notes = require('./db/simDB').initialize(data); 
-const simDB = require('./db/simDB');
-const notes = simDB.initialize(data);
 
 // Initialize an express app
 const express = require('express');
@@ -17,61 +13,8 @@ app.use(morgan('dev'));
 // Serve public files
 app.use('/', express.static('public'));
 
-// Parse incoming JSON
-app.use(express.json());
-
-// Add /notes/ endpoints
-app.get('/api/notes', (req, res, next) => {
-  // Fetch searchTerm query from client request
-  const {searchTerm} = req.query;
-  // Check if a search was requested
-  notes.filter(searchTerm, (err, list) => {
-    // Throw any errors
-    if (err) next(err);
-    // Send a response if no errors
-    else res.json(list);
-  });
-});
-
-app.get('/api/notes/:id', (req, res, next) => {
-  // fetch the ID of the requested item
-  const id = req.params.id;
-  // Locate the item in our database
-  notes.find(id, (err, item) => {
-    // Throw any errors
-    if (err) next(err);
-    // If item is found, respond to request
-    else if (item !== undefined) res.json(item);
-    // Otherwise, return 404
-    else next();
-  });
-});
-
-// Think about composing a function that returns a doctorObject() function
-  // Think of composition as combining abilities
-  // Factory function pattern?
-app.put('/api/notes/:id', (req, res, next) => {
-  // grab the ID from the request
-  const id = req.params.id;
-  // Create the object which will update our DB
-  const updateObj = {};
-  // Set the legal update fields
-  const updateFields = ['title', 'content'];
-  // Iterate through legal update fields
-  updateFields.forEach(field => {
-    // If the field is in our request
-    if (field in req.body) {
-      // Push the value of the field into our update obj
-      updateObj[field] = req.body[field];
-    }
-  });
-  // update the DB with user input
-  notes.update(id, updateObj, (err, item) => {
-    if (err) next(err);
-    else if (item !== undefined) res.json(item);
-    else next();
-  });
-});
+// Handle /api/items routes
+app.use('/api/notes', notesRouter);
 
 // Handle invalid requests
 app.use((req, res, next) => {
@@ -81,8 +24,8 @@ app.use((req, res, next) => {
   next(err);
 });
 
-// Send all error responses
-app.use( (err, req, res, next) => {
+// Respond to all errors
+app.use((err, req, res, next) => {
   res.status(err.status || 500); 
   res.json({
     message: err.message,
